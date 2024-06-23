1c45ece366ae184c04e6e47d8d3121b3e859f58b
“Ebenezeri <cebendemalek@gmail.com>
2024-06-23 12:38:48 +0300

    History graph

--- lib/Pages/history_page.dart
+++ lib/Pages/history_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:machine_monitory/data/database_helper.dart';
 import 'package:intl/intl.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
@@ -38,42 +39,163 @@ class _HistoryPageState extends State<HistoryPage> {
     return formatter.format(localTime);
   }
 
+  Map<String, double> getLatestValuesPerDay(List<Map<String, dynamic>> data) {
+    Map<String, double> latestValues = {};
+
+    for (var record in data) {
+      DateTime date = DateTime.parse(record['timestamp']).toLocal();
+      String dateString = DateFormat('yyyy-MM-dd').format(date);
+      double value = record['value'];
+
+      if (!latestValues.containsKey(dateString)) {
+        latestValues[dateString] = value;
+      } else {
+        latestValues[dateString] = value;
+      }
+    }
+
+    return latestValues;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('History'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    Map<String, double> latestTempValues = getLatestValuesPerDay(temperatureData);
+    Map<String, double> latestPressValues = getLatestValuesPerDay(pressureData);
+
+    List<BarChartGroupData> temperatureBarGroups = latestTempValues.entries.map((entry) {
+      return BarChartGroupData(
+        x: DateTime.parse(entry.key).millisecondsSinceEpoch,
+        barRods: [
+          BarChartRodData(
+            toY: entry.value,
+            color: Colors.red,
+            width: 15,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ],
+      );
+    }).toList();
+
+    List<BarChartGroupData> pressureBarGroups = latestPressValues.entries.map((entry) {
+      return BarChartGroupData(
+        x: DateTime.parse(entry.key).millisecondsSinceEpoch,
+        barRods: [
+          BarChartRodData(
+            toY: entry.value,
+            color: Colors.blue,
+            width: 15,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ],
+      );
+    }).toList();
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('History'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'List'),
+              Tab(text: 'Graphs'),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            const Text('Temperature Data', style: TextStyle(fontSize: 20)),
-            Expanded(
-              child: ListView.builder(
-                itemCount: temperatureData.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('${temperatureData[index]['value']} °C'),
-                    subtitle: Text(
-                        formatTimestamp(temperatureData[index]['timestamp'])),
-                  );
-                },
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Temperature Data', style: TextStyle(fontSize: 20)),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: temperatureData.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('${temperatureData[index]['value']} °C'),
+                          subtitle: Text(formatTimestamp(temperatureData[index]['timestamp'])),
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('Pressure Data', style: TextStyle(fontSize: 20)),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: pressureData.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('${pressureData[index]['value']} Pa'),
+                          subtitle: Text(formatTimestamp(pressureData[index]['timestamp'])),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 20),
-            const Text('Pressure Data', style: TextStyle(fontSize: 20)),
-            Expanded(
-              child: ListView.builder(
-                itemCount: pressureData.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('${pressureData[index]['value']} Pa'),
-                    subtitle:
-                        Text(formatTimestamp(pressureData[index]['timestamp'])),
-                  );
-                },
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Temperature Data', style: TextStyle(fontSize: 20)),
+                  Expanded(
+                    child: BarChart(
+                      BarChartData(
+                        minY: 0,
+                        maxY: 300,
+                        alignment: BarChartAlignment.spaceAround,
+                        barGroups: temperatureBarGroups,
+                        titlesData: FlTitlesData(
+                          topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: (double value, TitleMeta meta) {
+                                String date = DateFormat('MM-dd').format(DateTime.fromMillisecondsSinceEpoch(value.toInt()));
+                                return SideTitleWidget(
+                                  axisSide: meta.axisSide,
+                                  child: Text(date),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('Pressure Data', style: TextStyle(fontSize: 20)),
+                  Expanded(
+                    child: BarChart(
+                      BarChartData(
+                        minY: 0,
+                        maxY: 1000,
+                        alignment: BarChartAlignment.spaceAround,
+                        barGroups: pressureBarGroups,
+                        titlesData: FlTitlesData(
+                          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: (double value, TitleMeta meta) {
+                                String date = DateFormat('MM-dd').format(DateTime.fromMillisecondsSinceEpoch(value.toInt()));
+                                return SideTitleWidget(
+                                  axisSide: meta.axisSide,
+                                  child: Text(date),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
@@ -82,3 +204,5 @@ class _HistoryPageState extends State<HistoryPage> {
     );
   }
 }
+
+
